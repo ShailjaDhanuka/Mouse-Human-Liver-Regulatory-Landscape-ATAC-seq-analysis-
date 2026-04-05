15adf43bd053fa593ad8820f539da3d2daa6d142
Sophia <sophiat1101@gmail.com>
2026-04-05 16:28:17 -0400

    made ortholog mapping more reproducible for pipeline use

--- step2_cross_species_mapping/orthologMapping.sh
+++ step2_cross_species_mapping/orthologMapping.sh
@@ -7,46 +7,73 @@
 #SBATCH -t 09:00:00
 #SBATCH --cpus-per-task=2
 
-# loading in anaconda3 module  and hal envs
-module load anaconda3
-source activate hal
+# usage:
+# sbatch orthologMappping.sh /
+#     <MouseAtac.narrowPeak> \
+#     <HumanAtac.narrowPeak> \
+#     <12mammalianCactus> \
+#     <outputDirectory> \
 
-# making local working directory for job run
+MOUSE_ATAC="$1"
+HUMAN_ATAC="$2"
+CACTUS_ALIGN="$3"
+OUT_DIR="$4"
+
+# NOTE! -- HALPER_DIR must be set to path of halLiftover-processing repo
+# example: export HALPER_DIR=/ocean/projects/bio23000p/USER/halLiftover-postprocessing
+
+if [[ -z "$HALPER_DIR" ]]; then
+      echo "Error: HALPER_DIR is not set in script." >&2
+      echo "Set it to path of halLiftover-postprocessing repo." >&2
+      echo "Ex. export HALPER_DIR=/path/to/halLiftover-postprocessing?" >&2
+      exit 1
+fi
+
+HALPER_SCRIPT="$HALPER_DIR/halper_map_peak_orthologs.sh"
+
+# NOTE! - must have hal environment installed from halLiftover-postprocessing instructions
+# or create env from this repo (ex. conda env create -f halEnv.yml)
+
+# loading in anaconda3 module and hal envs
+module load anaconda3
+conda activate hal
+
+# making local node working directory for job run
 mkdir -p $LOCAL/mappingRun
 
 echo "Copying narrowPeak files to local node..."
-cp /ocean/projects/bio230007p/ikaplow/MouseAtac/Liver/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz $LOCAL/mappingRun/mouse.NarrowPeak.gz
-cp /ocean/projects/bio230007p/ikaplow/HumanAtac/Liver/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz $LOCAL/mappingRun/human.NarrowPeak.gz
+cp $MOUSE_ATAC $LOCAL/mappingRun/mouse.NarrowPeak.gz
+cp $HUMAN_ATAC $LOCAL/mappingRun/human.NarrowPeak.gz
 echo "Done copying files to local node."
 
 # in case job fails, sending any results back to project folder
 trap 'echo "Copying results back..."; \
-      cp $LOCAL/mappingRun/MouseAtacToHumanOrtho* /ocean/projects/bio230007p/sturecki/project/mappingResults/ 2>/dev/null; \
-      cp $LOCAL/mappingRun/HumanAtacToMouseOrtho* /ocean/projects/bio230007p/sturecki/project/mappingResults/ 2>/dev/null; \
+      cp $LOCAL/mappingRun/MouseAtacToHumanOrtho* $OUT_DIR 2>/dev/null; \
+      cp $LOCAL/mappingRun/HumanAtacToMouseOrtho* $OUT_DIR 2>/dev/null; \
       echo "Done."' EXIT
 
 # running halLiftover/HALPER script in parallel for mouse->human and human->mouse
-bash /jet/home/sturecki/practicum/projectScripts/halper_map_peak_orthologs.sh \
+bash $HALPER_SCRIPT \
 -b $LOCAL/mappingRun/mouse.NarrowPeak.gz \
 -o $LOCAL/mappingRun \
 -s Mouse \
 -t Human \
 -n MouseAtacToHumanOrtho \
--c /ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal &
+-c $CACTUS_ALIGN &
 PID1=$!
 
-bash /jet/home/sturecki/practicum/projectScripts/halper_map_peak_orthologs.sh \
+bash $HALPER_SCRIPT \
 -b $LOCAL/mappingRun/human.NarrowPeak.gz \
 -o $LOCAL/mappingRun \
 -s Human \
 -t Mouse \
 -n HumanAtacToMouseOrtho \
--c /ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal &
+-c $CACTUS_ALIGN &
 PID2=$!
 
 wait $PID1 $PID2
 
-echo "Copying intermediate and outputs back to project folder..."
-cp $LOCAL/mappingRun/MouseAtacToHumanOrtho* /ocean/projects/bio230007p/sturecki/project/mappingResults/
-cp $LOCAL/mappingRun/HumanAtacToMouseOrtho* /ocean/projects/bio230007p/sturecki/project/mappingResults/
+echo "Copying intermediate files and outputs back to project folder..."
+cp $LOCAL/mappingRun/MouseAtacToHumanOrtho* $OUT_DIR
+cp $LOCAL/mappingRun/HumanAtacToMouseOrtho* $OUT_DIR
 echo "Done"
